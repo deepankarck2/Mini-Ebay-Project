007177f6ed4ea764c96d9e18bf779531061b2eea
Deepankar <dip2leo@gmail.com>
2022-11-19 18:31:20 -0500

    added category page

--- MiniEbay/urls.py
+++ MiniEbay/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 from django.urls import include
 from users import views as user_views
 from django.contrib.auth import views as auth_views 
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
@@ -28,3 +29,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.profile, name='profile'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from .models import Category, Product
 def home(request):
     return render(request, 'store/home.html')
 
 def categories(request):
-    return render(request, 'store/categories.html')
+    category = Category.objects.filter(status=0)
+    context = {
+        'category' : category
+    }
+    return render(request, 'store/categories.html', context)
