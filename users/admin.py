2ceff648738d3fa243f37962398c6936591ec58f
Deepankar <dip2leo@gmail.com>
2022-12-08 21:26:17 -0500

    added blacklist view

--- users/admin.py
+++ users/admin.py
@@ -3,3 +3,4 @@ from .models import *
 # Register your models here.
 admin.site.register(Account)
 admin.site.register(ReviewRating)
+admin.site.register(UserBlacklist)
--- users/views.py
+++ users/views.py
@@ -3,9 +3,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm, AccountUpdateForm
 from django.contrib.auth.decorators import login_required
-
+from django.http import HttpResponse
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.views import LoginView
+from django.db.models import Q
+from .models import UserBlacklist
 
 #View related to login and show message
 class viewLogin(SuccessMessageMixin, LoginView):
@@ -15,13 +17,24 @@ class viewLogin(SuccessMessageMixin, LoginView):
 
 # View related to register
 def register(request):
+    if (request.user.is_authenticated):
+        return redirect('store-home')
+        
     if(request.method == "POST"):
         form = UserRegisterForm(request.POST)
         if (form.is_valid()):
-            form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}! Please Login')
-            return redirect('login')
+            email = form.cleaned_data.get('email')
+            black_listed = UserBlacklist.objects.all()
+            check_user = black_listed.filter(Q(username=username) | Q(email=email))
+
+            if(check_user):
+                return HttpResponse("<h1 style='color:red;'>You are in blacklist! Can't create account! </h1>") 
+            else:
+                form.save()
+                username = form.cleaned_data.get('username')
+                messages.success(request, f'Account created for {username}! Please Login')
+                return redirect('login')
         else:
             return render(request, 'users/register.html', {'form' : form})
     else:
